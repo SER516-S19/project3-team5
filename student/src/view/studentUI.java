4f44996e1280795511340c06e917ddceb0fb26fc
HalcyonAura <laplace95@gmail.com>
2019-04-01 18:19:53 -0700

    Panel added. Testing in progress

--- student/src/view/studentUI.java
+++ student/src/view/studentUI.java
@@ -1,13 +1,16 @@
 package view;
 import javax.swing.*;
 
-public class studentUI extends JFrame {
+public class studentUI JFrame {
     public studentUI(){
-        super("Quiz Taker");
-        setSize(800, 600);
-        setLayout(null);
-        setVisible(true);
-        //TODO: The quiz selection panel needs to be added here.
+        JFrame studentApp = new JFrame();
+        studentApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        studentApp.setSize(800, 600);
+        studentApp.setTitle("Student Quiz Taker");
+        studentApp.setLayout(new BorderLayout());
+        JPanel quizPanel = new JPanel(new BorderLayout());
+        studentApp.add(quizPanel, BorderLayout.CENTER);
+        studentApp.setVisible(true);
     }
 
     public static void main(String[] args){
