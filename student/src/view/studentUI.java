0377f115d02720d87d0746ee90e0526c46557329
dlahtine <dlahtine@asu.edu>
2019-04-01 17:07:44 -0700

    frame for student application

--- student/src/view/studentUI.java
+++ student/src/view/studentUI.java
@@ -1,4 +1,16 @@
 package view;
+import javax.swing.*;
 
-public class studentUI {
+public class studentUI extends JFrame {
+    public studentUI(){
+        super("Quiz Taker");
+        setSize(600, 800);
+        setLayout(null);
+        setVisible(true);
+        //TODO: The quiz selection panel needs to be added here.
+    }
+
+    public static void main(String[] args){
+        new studentUI();
+    }
 }
