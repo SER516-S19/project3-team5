972b76963f37cebf80af23849508deaa4768104f
Cecilia La Place <laplace95@gmail.com>
2019-04-01 18:41:43 -0700

    Added panel and button

--- student/src/view/StudentUI.java
+++ student/src/view/StudentUI.java
@@ -1,19 +1,21 @@
 package view;
+import java.awt.BorderLayout;
 import javax.swing.*;
 
-public class studentUI JFrame {
-    public studentUI(){
+public class StudentUI {
+    public StudentUI(){
+
         JFrame studentApp = new JFrame();
         studentApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         studentApp.setSize(800, 600);
         studentApp.setTitle("Student Quiz Taker");
-        studentApp.setLayout(new BorderLayout());
-        JPanel quizPanel = new JPanel(new BorderLayout());
+        JPanel quizPanel = new JPanel();
+        quizPanel.add(new GiveUpButton(), BorderLayout.CENTER);
         studentApp.add(quizPanel, BorderLayout.CENTER);
         studentApp.setVisible(true);
     }
 
     public static void main(String[] args){
-        new studentUI();
+        new StudentUI();
     }
 }
