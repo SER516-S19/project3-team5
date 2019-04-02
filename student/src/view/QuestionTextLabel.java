03ddeb5bb396148eabdc5060c163525add93d7aa
arch92 <arch92@gmail.com>
2019-04-02 16:10:35 -0700

    Question Text Label
    Task#30 Create Label to show question Text

---
+++ student/src/view/QuestionTextLabel.java
@@ -0,0 +1,27 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.*;
+
+
+/**
+ * 
+ * @author Archana Madhavan
+ * @version 
+ * @since
+ *
+ */
+public class QuestionTextLabel extends JLabel{
+
+
+	 QuestionTextLabel() {
+		super("This is a question for a student to answer to choose \n");
+		this.setBounds(100,100,100,100);
+		
+		 
+	}
+	
+	
+}
--- student/src/view/StudentUI.java
+++ student/src/view/StudentUI.java
@@ -10,6 +10,8 @@ public class StudentUI {
         studentApp.setSize(800, 600);
         studentApp.setTitle("Student Quiz Taker");
         JPanel quizPanel = new JPanel();
+        //#30 Create Label to show question Content
+        quizPanel.add(new QuestionTextLabel(),BorderLayout.CENTER);
         quizPanel.add(new GiveUpButton(), BorderLayout.CENTER);
         quizPanel.add(new SubmitButton(), BorderLayout.CENTER);
         studentApp.add(quizPanel, BorderLayout.CENTER);
