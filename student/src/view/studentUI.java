639b33d7f50f703d54dd5879439da68830f0a4ca
dlahtine <dlahtine@asu.edu>
2019-04-01 20:05:49 -0700

    Quiz selection panel is a bit ugly, but definitely functional.

--- student/src/view/studentUI.java
+++ student/src/view/studentUI.java
@@ -5,11 +5,12 @@ public class studentUI extends JFrame {
     public studentUI(){
         super("Quiz Taker");
         setSize(800, 600);
-        setVisible(true);
         //TODO: The quiz selection panel needs to be added here.
     }
 
     public static void main(String[] args){
-        new studentUI();
+        studentUI ui = new studentUI();
+        ui.add(new QuizSelectionPanel());
+        ui.setVisible(true);
     }
 }
