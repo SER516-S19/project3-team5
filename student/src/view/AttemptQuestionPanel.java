a22e30c6e342cb2901d3ad733981c0ba98287ca1
arch92 <arch92@gmail.com>
2019-04-05 16:52:45 -0700

    Refactoring StudentUI

--- student/src/view/AttemptQuestionPanel.java
+++ student/src/view/AttemptQuestionPanel.java
@@ -1,33 +1,124 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 
 /**
  * Class that provides UI for student to attempt quiz
  * @author Archana Madhavan
- * @version 
- * @since
+ * @version 1.1
+ * @since 4/4/2019
  *
  */
 public class AttemptQuestionPanel extends JPanel{
-	JPanel questionPanel=new JPanel();
+	private static final int SUBMIT = 0;
+	private static final int GIVEUP = 1;
+	private static final int ANSWERLENGTH = 4;
+	private char answerOptions[]= {'A','B','C','D'};
+	
 	AttemptQuestionPanel() {
 		super();
-        setLayout(new BorderLayout());
-        addQuestionTextPane(); 
+        this.setLayout(new BorderLayout());
+        JPanel questionPanel = addQuestionLabelPanel();
+        JPanel navigateButtonPanel = addnavigateButtonPanel();
+        JScrollPane answersPanel = new JScrollPane();
+        AnswerOptionsPanel optionPanels[]= new AnswerOptionsPanel[ANSWERLENGTH];
+        ButtonGroup answerGroup = new ButtonGroup();
+        
+        //grouping radio buttons and adding labels to question panel.
+        for (int i = 0; i < ANSWERLENGTH; i++){
+        	optionPanels[i] = new AnswerOptionsPanel();
+            answerGroup.add(optionPanels[i].getRadioButton());
+            optionPanels[i].getRadioButton().setText(Character.toString(answerOptions[i]));
+            questionPanel.add(optionPanels[i]);
+        }
+        
+        this.add(questionPanel, BorderLayout.NORTH);
+        this.add(answersPanel, BorderLayout.CENTER);
+        this.add(navigateButtonPanel, BorderLayout.SOUTH);
+	  
+        
 	}
 	
-	private void addQuestionTextPane()
+	/**
+     * Creates the navigate panel
+     * @return navigateButtonPanel
+     */
+	private JPanel addQuestionLabelPanel()
     {
-		questionPanel.setLayout(new BoxLayout(questionPanel, BoxLayout.Y_AXIS));
-		questionPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-    	//#30 Create Label to show question Content
-    	JLabel questionTextLabel=new JLabel("This is a question for a student to answer to choose");
-    	questionTextLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JLabel questionTextLabel;
+		JPanel questionPanel=new JPanel();
+		questionPanel.setPreferredSize(new Dimension(700, 300));
+    	questionTextLabel=new JLabel("This is a question for a student to answer to choose");
     	questionPanel.add(questionTextLabel);
-    	add(questionPanel, BorderLayout.CENTER);
+    	return questionPanel;
     }
+	
+	/**
+     * Creates the navigate panel
+     * @return navigateButtonPanel
+     */
+	private JPanel addnavigateButtonPanel()
+	{
+		JPanel navigateButtonPanel = new JPanel();
+		navigateButtonPanel.setLayout(new GridLayout(2, 2));
+		navigateButtonPanel.setPreferredSize(new Dimension(400, 100));
+		JButton giveUpButton=addNavigateButton(GIVEUP);
+		JButton submitButton=addNavigateButton(SUBMIT);
+		
+		giveUpButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+		submitButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+		navigateButtonPanel.add(giveUpButton);
+		navigateButtonPanel.add(submitButton);
+        
+    	return navigateButtonPanel;
+	}
+	    
+    /**
+     * Creates custom navigate buttons
+     * @return navigateButton
+     */
+    public JButton addNavigateButton(int btnType){
+        String name = "";
+        Color btnColor = null;
+        if (btnType == GIVEUP ){
+            name = "Give Up";
+            btnColor = Color.RED;
+        }
+        else if (btnType == SUBMIT){
+            name = "Submit";
+            btnColor = Color.GREEN;
+        }
+        else{
+            return null; 
+        }
+       
+        JButton navigateButton = new JButton(name);
+        navigateButton.setBounds(50, 100, 95, 30);
+        navigateButton.setLayout(null);
+        navigateButton.setVisible(true);
+        navigateButton.setBackground(btnColor);
+        navigateButton.setOpaque(true);
+        navigateButton.setBorderPainted(false);
+        return navigateButton;
+    }
+		
 }
--- student/src/view/StudentUI.java
+++ student/src/view/StudentUI.java
@@ -12,19 +12,18 @@ import javax.swing.*;
  */
 
 public class StudentUI {
-	JPanel quizPanel = new JPanel();
-	AttemptQuestionPanel questionPane=new AttemptQuestionPanel();
+	
+	
     public StudentUI(){
 
         JFrame studentApp = new JFrame();
         studentApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         studentApp.setSize(800, 600);
         studentApp.setTitle("Student Quiz Taker");
-                
-        quizPanel.add(new GiveUpButton(), BorderLayout.CENTER);
-        quizPanel.add(new SubmitButton(), BorderLayout.CENTER);
-        studentApp.add(quizPanel, BorderLayout.CENTER);
-        studentApp.add(questionPane,BorderLayout.CENTER);
+          
+        studentApp.add(new AttemptQuestionPanel());
+        studentApp.pack();
+        studentApp.setLocationRelativeTo(null);
         studentApp.setVisible(true);
     }
     
