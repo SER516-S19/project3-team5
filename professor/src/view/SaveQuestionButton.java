2cb46f06096ad3c483dab759a7b8b339b012f317
hsinjlee <rlee4408@gmail.com>
2019-04-10 06:57:09 -0700

    updated save question connect to DAO

--- professor/src/view/SaveQuestionButton.java
+++ professor/src/view/SaveQuestionButton.java
@@ -1,152 +1,51 @@
-package gui;
-
-import controller.Controller;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.io.IOException;
 
-/*
-Name: Hsin-Jung Lee
-Version: 3
+/**
+ * This class show SaveQuestionButton and actionlistener
+ * Author: Hsin-Jung Lee
+ * Version: 4
  */
 
-public class SaveQuestionButton extends JFrame {
 
-	private TextPanel textPanel;
-	private Toolbar toolbar;
-	private FormPanel formPanel;
-	private JFileChooser fileChooser;
-	private Controller controller;
-	private TablePanel tablePanel;
+public class SaveQuestionButton {
+    static JFrame frame;
 
-	public SaveQuestionButton() {
-		super("Professor");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                displaySaveQuestionButton();
+            }
+        });
+    }
 
-		setLayout(new BorderLayout());
+    static void displaySaveQuestionButton() {
+        frame = new JFrame("Professor");
 
-		toolbar = new Toolbar();
-		textPanel = new TextPanel();
-		formPanel = new FormPanel();
-		tablePanel = new TablePanel();
+        // create our jbutton
+        JButton saveQuestionButton = new JButton("Save Question");
 
-		controller = new Controller();
+        // add the listener to the jbutton to handle the "pressed" event
+        saveQuestionButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // display/center the jdialog when the button is pressed
+                JDialog saveQuestion = new JDialog(frame, "Save Question", true);
+                saveQuestion.setLocationRelativeTo(frame);
+                saveQuestion.setVisible(true);
+            }
+        });
 
-		tablePanel.setData(controller.getPeople());
+        // put the button on the frame
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.add(saveQuestionButton);
 
-		fileChooser = new JFileChooser();
-
-		fileChooser.addChoosableFileFilter(new PersonFileFilter());
-
-		setJMenuBar(createMenuBar());
-
-		toolbar.setStringListener(new StringListener() {
-			public void textEmitted(String text) {
-				textPanel.appendText(text);
-			}
-		});
-
-		formPanel.setFormListener(new FormListener() {
-			public void formEventOccurred(FormEvent e) {
-				controller.addPerson(e);
-				tablePanel.refresh();
-			}
-		});
-
-		add(formPanel, BorderLayout.WEST);
-		add(toolbar, BorderLayout.NORTH);
-		add(tablePanel, BorderLayout.CENTER);
-
-		setMinimumSize(new Dimension(500, 400));
-		setSize(600, 500);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-	}
-
-	private JMenuBar createMenuBar() {
-		JMenuBar menuBar = new JMenuBar();
-
-		JMenu fileMenu = new JMenu("File");
-		JMenuItem exportDataItem = new JMenuItem("Export Data...");
-		JMenuItem importDataItem = new JMenuItem("Import Data...");
-		JMenuItem exitItem = new JMenuItem("Exit");
-
-		fileMenu.add(exportDataItem);
-		fileMenu.add(importDataItem);
-		fileMenu.addSeparator();
-		fileMenu.add(exitItem);
-
-		JMenu windowMenu = new JMenu("Window");
-		JMenu showMenu = new JMenu("Show");
-
-		JCheckBoxMenuItem showFormItem = new JCheckBoxMenuItem("Quiz Form");
-		showFormItem.setSelected(true);
-
-		showMenu.add(showFormItem);
-		windowMenu.add(showMenu);
-
-		menuBar.add(fileMenu);
-		menuBar.add(windowMenu);
-
-		showFormItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ev) {
-				JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem) ev.getSource();
-
-				formPanel.setVisible(menuItem.isSelected());
-			}
-		});
-
-		fileMenu.setMnemonic(KeyEvent.VK_F);
-		exitItem.setMnemonic(KeyEvent.VK_X);
-
-		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
-				ActionEvent.CTRL_MASK));
-
-		importDataItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (fileChooser.showOpenDialog(SaveQuestionButton.this) == JFileChooser.APPROVE_OPTION) {
-					try {
-						controller.loadFromFile(fileChooser.getSelectedFile());
-						tablePanel.refresh();
-					} catch (IOException e1) {
-						JOptionPane.showMessageDialog(SaveQuestionButton.this,
-								"Could not load data from file.", "Error",
-								JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}
-		});
-
-		exportDataItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (fileChooser.showSaveDialog(SaveQuestionButton.this) == JFileChooser.APPROVE_OPTION) {
-					try {
-						controller.saveToFile(fileChooser.getSelectedFile());
-					} catch (IOException e1) {
-						JOptionPane.showMessageDialog(SaveQuestionButton.this,
-								"Could not save data to file.", "Error",
-								JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}
-		});
-
-		exitItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-
-				int action = JOptionPane.showConfirmDialog(SaveQuestionButton.this,
-						"Do you really want to exit the application?",
-						"Confirm Exit", JOptionPane.OK_CANCEL_OPTION);
-
-				if (action == JOptionPane.OK_OPTION) {
-					System.exit(0);
-				}
-			}
-		});
-
-		return menuBar;
-	}
+        // set up the jframe, then display it
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(600, 500));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
