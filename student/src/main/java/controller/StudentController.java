c01f28cffa2392d715d72b23aa659709cf2633d3
hkolli <hkolli@asu.edu>
2019-04-10 19:05:27 -0700

    Fetched all the values from JSON.

--- student/src/main/java/controller/StudentController.java
+++ student/src/main/java/controller/StudentController.java
@@ -4,44 +4,54 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.awt.*;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class StudentController {
-	
-	public static void updateQuizPage() throws Exception
-	{
+
+	public static Map<Integer,ArrayList> updateQuizPage() throws Exception {
 		System.out.println("In update Quiz");
 		JSONParser parser = new JSONParser();
+
+		Map<Integer,ArrayList> questionMap = new HashMap<>();
 		try {
 
-            Object obj = parser.parse(new FileReader("C:\\Users\\Arch\\Documents\\GitHub\\project3-team5\\quizzes\\SampleQuiz.json"));
+			Object obj = parser.parse(new FileReader("C:\\SER516\\project3-team5\\quizzes\\SampleQuiz.json"));
 
-            JSONObject jsonObject = (JSONObject) obj;
-                      
-            JSONArray questions= (JSONArray)jsonObject.get("questions");
-            for(int i=0;i<questions.size();i++) {
-            	JSONObject question=(JSONObject)questions.get(i);
-            	ArrayList options=(ArrayList)question.get("options");
-            	System.out.println(options);
-            	String questionTitle=(String)question.get("title");
-            	System.out.println(questionTitle);
-            	String correctAnswer=(String)question.get("correctAnswer");
-            	System.out.println(correctAnswer);
-           }
-            	
-      	 
+			JSONObject jsonObject = (JSONObject) obj;
+
+			JSONArray questions = (JSONArray) jsonObject.get("questions");
+
+			for (int i = 0; i < questions.size(); i++) {
+                ArrayList<String> al = new ArrayList<>();
+				JSONObject question = (JSONObject) questions.get(i);
+				String questionTitle = (String) question.get("title");
+				ArrayList options = (ArrayList) question.get("options");
+				String correctAnswer = (String) question.get("correctAnswer");
+
+				al.add(questionTitle);
+				for(int k=0;k<options.size();k++)
+					al.add(options.get(k).toString());
+				al.add(correctAnswer);
+				questionMap.put(i,al);
             }
-            
+
+		}
+
 		catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-}
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return questionMap;
 	}
+}
--- student/src/main/java/view/AttemptQuestionPanel.java
+++ student/src/main/java/view/AttemptQuestionPanel.java
@@ -2,12 +2,13 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.*;
 
@@ -55,15 +56,16 @@ public class AttemptQuestionPanel extends JPanel{
      */
 	private JPanel addQuestionLabelPanel()
     {
-		
+
 		JLabel questionTextLabel;
 		JPanel questionPanel=new JPanel();
 		questionPanel.setPreferredSize(new Dimension(700, 300));
+
     	questionTextLabel=new JLabel("This is a question for a student to answer to choose");
     	questionPanel.add(questionTextLabel);
     	return questionPanel;
     }
-	
+
 	/**
      * Creates the navigate panel
      * @return navigateButtonPanel
@@ -83,8 +85,23 @@ public class AttemptQuestionPanel extends JPanel{
         });
 		submitButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
+                ArrayList<String> listNew = new ArrayList<>();
             	try {
-					controller.StudentController.updateQuizPage();
+                    Map<Integer,ArrayList> list = controller.StudentController.updateQuizPage();
+                    System.out.println(list);
+
+                   for(int l=0;l<list.size();l++)
+                   {
+                       ArrayList question = new ArrayList(list.get(l));
+                       System.out.println(question);
+                       for(int questionValue=0;questionValue<question.size();questionValue++)
+                       {
+                           System.out.println(question.get(questionValue));
+
+                       }
+                   }
+
+
 				} catch (Exception e1) {
 					System.out.println("Exception has been occured on submit");
 				}
