c9f644169805cedf76fe6f1ec4850f11f8f839b2
mspranav <35428943+mspranav@users.noreply.github.com>
2019-04-11 15:53:50 -0700

    End of the Quiz #15 and #32

---
+++ student/src/main/java/view/QuizCompletedPanel.java
@@ -0,0 +1,32 @@
+package view;
+
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author Siva Pranav Mandadi,
+ * @version 1.1
+ * 
+ */
+
+
+//call the constructor for the panel
+public class QuizCompletedPanel{
+	public QuizCompletedPanel(JFrame studentFrame){
+		super();
+		this.studentFrame=studentFrame;
+		JPanel quizcompletedpanel = new JPanel();
+		JLabel label = new JLabel("Quiz has been completed");
+        quizcompletedpanel.setSize(800, 600);
+        quizcompletedpanel.add(label);
+        this.add(quizcompletedpanel, BorderLayout.CENTER);
+
+
+	}
+
+
+}
